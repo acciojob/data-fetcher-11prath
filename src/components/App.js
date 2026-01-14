73f9e467e7cafe330f936e9cdcde8c8953279b8f
PrathviShetty <108prathvi.shetty@gamil.com>
2026-01-14 16:40:06 +0530

    Completed Button Counter assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
+import React, { useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [count, setCount] = useState(0);
 
-const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
